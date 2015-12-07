70ed774a39e79fb3548787445142a4e6021a5321
Piers Williams <male@nospam@piers7.com>
2015-12-07 15:45:01 +0800

    Move SQL files to sub-folder

---
+++ Install/SQL/Add-SqlAgentJobSchedule.ps1
@@ -0,0 +1,43 @@
+﻿param
+(    
+    [Parameter(Mandatory=$true)] [Microsoft.SqlServer.Management.Smo.Agent.Job] $job, 
+    [TimeSpan] $frequency,
+    $frequencyType = 'Daily', # Unknown, OneTime, Daily, Weekly, Monthly, MonthlyRelative, AutoStart, OnIdle    [datetime] $startDate = $((Get-Date).Date),
+    [timespan] $startTime = '00:00:00',
+    [datetime] $endDate = '2099-01-01',
+    [timespan] $endTime = '23:59:59',
+    [switch] $force
+)
+
+if($force){
+    $job.RemoveAllJobSchedules();
+}
+    
+$schedule = New-Object Microsoft.SqlServer.Management.Smo.Agent.JobSchedule $job,'Schedule';
+$schedule.FrequencyTypes = $frequencyType;
+$schedule.FrequencyInterval = 1;
+$schedule.FrequencyRecurrenceFactor = 1;
+
+if($frequency -le [timespan]::Zero){
+    # Once only
+    $schedule.FrequencySubDayTypes = 'Once'; 
+    $scheduleName = "Once $($frequencyType)";
+}elseif($frequency -lt [TimeSpan]::FromHours(1)){
+    # Minutes
+    $schedule.FrequencySubDayInterval = $frequency.TotalMinutes;
+    $schedule.FrequencySubDayTypes = 'Minute';
+    $scheduleName = "Every $($schedule.FrequencySubDayInterval) $($schedule.FrequencySubDayTypes)s";
+}else{
+    # Assume hours
+    $schedule.FrequencySubDayInterval = $frequency.TotalHours;
+    $schedule.FrequencySubDayTypes = 'Hour';
+    $scheduleName = "Every $($schedule.FrequencySubDayInterval) $($schedule.FrequencySubDayTypes)s";
+}
+
+Write-Verbose "[Sql\$sqlInstance\$jobName] Add schedule '$scheduleName' (start $startTime)"    
+$schedule.Name = $scheduleName;
+$schedule.ActiveStartDate = $startDate.Date;
+$schedule.ActiveStartTimeOfDay = $startTime;
+$schedule.ActiveEndDate = $endDate.Date;
+$schedule.ActiveEndTimeOfDay = $endTime;
+$schedule.Create();
---
+++ Install/SQL/Add-SqlAgentJobStep.ps1
@@ -0,0 +1,47 @@
+﻿param(
+    [Parameter(Mandatory=$true)] [Microsoft.SqlServer.Management.Smo.Agent.Job] $job, 
+    [Parameter(Mandatory=$true)] $stepName,
+    [Parameter(Mandatory=$true)] $subSystem,
+    [Parameter(Mandatory=$true)] $command,
+    $databaseName,
+    $proxy,
+    $serverName,
+    $commandExecutionSuccessCode = 0,
+    [int] $retryAttempts = 0,
+    [int] $retryIntervalMins = 0,
+    $successAction = [Microsoft.SqlServer.Management.Smo.Agent.StepCompletionAction]::GoToNextStep,
+    $failureAction = [Microsoft.SqlServer.Management.Smo.Agent.StepCompletionAction]::QuitWithFailure,
+    [switch] $nonFatal
+)
+  
+Write-Verbose "[Sql\$sqlInstance\$jobName] Add step '$stepName' ($subSystem)"    
+Write-Verbose $command;
+
+$step = new-object Microsoft.SqlServer.Management.Smo.Agent.JobStep($job,$stepName);
+$step.SubSystem = $subSystem;
+$step.Command = $command;
+$step.RetryAttempts = $retryAttempts;
+$step.RetryInterval = $retryIntervalMins;
+    
+switch($subSystem){
+    'CmdExec' {
+        $step.CommandExecutionSuccessCode = 0;
+        break;
+    }
+}
+# appears unneccesary on Sql 2008
+# $step.JobStepFlags &= AppendAllCmdExecOutputToJobHistory 
+# http://msdn.microsoft.com/en-us/library/microsoft.sqlserver.management.smo.agent.jobstep.jobstepflags(v=sql.105).aspx
+
+if($proxy){
+    $step.ProxyName = $proxy;
+}
+if($databaseName){
+    $step.DatabaseName = $databaseName;
+}
+if($serverName){
+    $step.Server = $serverName;
+}
+$step.OnSuccessAction = $successAction;
+$step.OnFailAction = $failureAction;
+$step.Create();
