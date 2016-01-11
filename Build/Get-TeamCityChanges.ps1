11e9445824ec4e69d787adf62674bb2b91774252
piers7 <male@NOSPAM.piers7.com>
2016-01-12 06:53:34 +0800

    Generate release notes from TeamCity
    include cmdlet to generate release notes based on TeamCity REST api
    Extend TeamCity property loader to be able to use non-xml version of
    build properties file (though this doesn't actually seem to help - the
    property I needed still isn't in there)

---
+++ Build/Get-TeamCityChanges.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.synopsis
+Extracts checkin comments from TeamCity's REST api for a particular build (normally the current one)
+PW 2015
+#>
+[CmdLetBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    $buildId,
+    [Parameter(Mandatory=$true)]
+    $serverUri = 'http://teamcity:8070',
+    [switch] $asMarkdown
+)
+
+function Load-Xml($uri){
+    $response = Invoke-WebRequest -Uri:$uri;
+    [xml]($response.Content);
+}
+
+# Nice non-iterative version as per http://stackoverflow.com/a/25515487/26167
+
+$changes = Load-Xml "$serverUri/guestAuth/app/rest/changes?locator=build:(id:$buildId)&fields=count,change:(version,date,username,comment)"
+$changesParsed = $changes.SelectNodes('//change') | Select-Object Version,UserName,@{Name='date';Expression={[DateTime]::ParseExact($_.date, 'yyyyMMddTHHmmsszzzz', $null)}},Comment
+if(!$asMarkdown){
+    $changesParsed
+}else{
+    "# Release Notes";
+    ""
+    $changesParsed | % { "  - [{0:yyyyMMdd}] {1} ({2})" -f $_.date,$_.comment,$_.username }
+    ""
+    "[More Details]($serverUri/viewLog.html?buildId=$buildId)"
+}
--- Build/Get-TeamCityProperties.ps1
+++ Build/Get-TeamCityProperties.ps1
@@ -6,10 +6,7 @@ or an empty hashtable if not running under TeamCity
 [CmdLetBinding()]
 param(
     # Path to the .properties file for this build (determined automatically)
-    $file = $env:TEAMCITY_BUILD_PROPERTIES_FILE + ".xml",
-
-    # Whether or not the script is running under TeamCity (determined automatically)
-    [switch] $inTeamCity = (![String]::IsNullOrEmpty($env:TEAMCITY_VERSION)),
+    $file = $env:TEAMCITY_BUILD_PROPERTIES_FILE,
 
     # Whether TeamCity properties should be resolved (eval'd) as powershell expressions if they start with $
     # This enables objects to be passed in (eg arrays) that would otherwise just be strings
@@ -17,20 +14,37 @@ param(
 )
 
 $buildProperties = @{};
-if($inTeamCity){
+if($file){
     Write-Verbose "Loading TeamCity properties from $file"
     $file = (Resolve-Path $file).Path;
 
-    $buildPropertiesXml = New-Object System.Xml.XmlDocument
-    $buildPropertiesXml.XmlResolver = $null; # force the DTD not to be tested
-    $buildPropertiesXml.Load($file);
-    foreach($entry in $buildPropertiesXml.SelectNodes("//entry")){
+    if([IO.Path]::GetExtension($file) -eq '.xml'){
+        $buildPropertiesXml = New-Object System.Xml.XmlDocument
+        $buildPropertiesXml.XmlResolver = $null; # force the DTD not to be tested
+        $buildPropertiesXml.Load($file);
+
+        $buildPropertiesRaw = $buildPropertiesXml.SelectNodes("//entry") | Select-Object Key,@{Name='Value';Expression={$_.'#text'}}
+    }else{
+        # The XML file doesn't seem to have half the properties in it
+        # so resorting to bludgery to get them out of the text file version
+        $buildPropertiesRaw = Get-Content $file | % { 
+            $parts = $_ -split '=',2;
+            New-Object PSObject -Property:@{
+                Key = $parts[0];
+                Value = [Regex]::Unescape($parts[1]); # why everything is escaped in raw file I have no idea
+            }
+        }
+    }
+
+    foreach($entry in $buildPropertiesRaw){
         $key = $entry.key;
-        $value = $entry.'#text';
+        $value = $entry.value;
         if($value -and $value.StartsWith('$') -and $resolveExpressions){
             # This allows us to use PowerShell expression syntax to get strong-types into PowerShell
             $value = Invoke-Expression $value;
         }
+
+        Write-Verbose "`tLoaded $key = $value";
         $buildProperties[$key] = $value;
     }
 }
