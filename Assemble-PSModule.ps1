9f1eaf924b36e5d5e73745d8794fe3b70badfffb
piers7 <male@NOSPAM.piers7.com>
2016-01-12 06:54:37 +0800

    Include script module manifest (and version) in build

--- Assemble-PSModule.ps1
+++ Assemble-PSModule.ps1
@@ -1,24 +1,43 @@
 param(
-    [Parameter(Mandatory=$true)] $scriptFolder,
-    $moduleName = ('Pink-' + (Split-Path -Leaf $scriptFolder)),
-    $outputFolder = '.\output'
+    [Parameter(Mandatory=$true)] $manifestPath,
+    $outputFolder = '.\output',
+    $modulePrefix,
+    $version,
+    [switch]$noExports
 )
 
-$modulePath = "$outputFolder\$moduleName.psm1"
+$scriptDir = Split-Path (Convert-Path $MyInvocation.MyCommand.Path);
+
 if(!(Test-Path $outputFolder)){
     [void] (mkdir $outputFolder);
 }
+[void] (Resolve-Path $manifestPath);
+
+$manifestDirectory = Split-Path $manifestPath;
+$moduleName = [IO.Path]::GetFileNameWithoutExtension($manifestPath);
+$modulePath = Join-Path $outputFolder "$moduleName.psm1"
+if($modulePrefix){
+    $modulePrefix = $moduleName -split '-' | Select-Object -Last:1;
+}
+
+$manifestTargetPath = Join-Path $outputFolder (Split-Path -Leaf $manifestPath);
 
 Write-Host "Assembling $modulePath"
 "" | Set-Content $modulePath
 
-dir $scriptFolder *.ps1 | 
+# Include the manifest (with updated version number if required)
+Copy-Item $manifestPath $outputFolder -Force;
+if($version){
+    Get-Item $manifestTargetPath | & $scriptDir\build\Set-VersionNumber.ps1 $version
+}
+
+# Now assemble the module contents
+dir $manifestDirectory *.ps1 | 
     ? { !$_.Name.StartsWith('_') } |
     % { 
         $file = $_;
         Write-Verbose "Including $file"
-        Get-Content $file.FullName | ? { $_ -ne '[CmdLetBinding()]' } | # invalid in modules apparently
-        & {
+        Get-Content $file.FullName | & {
             begin {
                 $functionName = $_.BaseName; # file name without extension
                 $wroteHeader = $false;
@@ -26,21 +45,32 @@ dir $scriptFolder *.ps1 |
             }
             process {
                 $line = $_;
-                if($line.Contains('= Split-Path $MyInvocation.MyCommand.Path')){
-                    throw "$functionName - Can''t use MyInvocation.MyCommand.Path in a script module";
+                if($line.Contains('= Split-Path $MyInvocation.MyCommand.Path') -or $line.Contains('= Split-Path (Convert-Path $MyInvocation.MyCommand.Path)')){
+                    throw "$functionName - Can''t use MyInvocation.MyCommand.Path in a script module (use an if(`$PSScriptRoot){}else{} block instead)";
                 }
 
-                if(!$wroteHeader -and $line -match '\s*param\s*\('){
+                if(!$wroteHeader -and (($line -match '\s*param\s*\(') -or ($line -match '\[CmdLetBinding'))){
                     "function $functionName {"
                     $wroteHeader = $true;
+                }else{
+                    # Attempt to re-point inter-script references to functions within the module
+                    $internalCallPattern = ('\.\\(\w+\-{0}\w+)\.ps1' -f $modulePrefix);
+                    $line = $line -replace $internalCallPattern,'$1';
                 }
+                if($line -match 'Export\-ModuleMember'){
+                    $noExports = $true;
+                }
+
+                # always emit original line (under current structure anyways)
                 $line;
             }
             end{
                 "";
                 if($wroteHeader){
                     "} # end $functionName";
-                    "Export-ModuleMember -function $functionName;"
+                    if(!$noExports){
+                        "Export-ModuleMember -function $functionName;"
+                    }
                 }else{
                     "# end $file"
                 }
@@ -50,5 +80,7 @@ dir $scriptFolder *.ps1 |
         } | Out-File -Append -FilePath:$modulePath -Encoding:ASCII;
     }
 
-# return the FileInfo back to the caller
-Get-Item $modulePath;
+# return the FileInfo back to the caller *of the manifest*
+# otherwise Manifest information is not loaded
+# nb: manifest *must* point back to the script module
+Get-Item $manifestTargetPath;
--- Build.ps1
+++ Build.ps1
@@ -11,39 +11,63 @@ $scriptDir = Split-Path (Convert-Path $MyInvocation.MyCommand.Path);
 
 function AssembleModule{
     param(
-        $moduleFolder,
-        $outputFolder
+        $manifestPath,
+        $outputFolder,
+        $version
     )
     # Basic assemble module stages
-    $moduleFile = .\Assemble-PsModule.ps1 $moduleFolder -outputFolder:$outputFolder;
-    $module = Import-Module $moduleFile.FullName -PassThru #-ErrorAction:stop;
+    $modulePath = .\Assemble-PsModule.ps1 $manifestPath -outputFolder:$outputFolder -version:$version;
 
-    # Dump out what modules we built
+    # Load the resulting module (check it basically works)
+    # ...and dump what we exported
+    $module = Import-Module $modulePath.FullName -PassThru -ErrorAction:Stop;
+    Write-Host "Generated module $($module.Name) v$($module.Version)"
+    Assert ($module.Version -gt '0.0') "Version not stamped correctly";
+
     $module | % {
         Write-Host "Module $($_.Name) contains";
         $_.ExportedCommands.Keys | % { "`t$_" };
     }
 }
 
+function Assert(
+    [Parameter(Mandatory=$true)]
+    $condition,
+    $failureMessage = '',
+    $successMessage
+)
+{
+    if (!$condition) {
+        throw "Assert Failed: $failureMessage";
+    }elseif($successMessage){
+        Write-Host $successMessage -ForegroundColor:Green;
+    }
+}
+
+Get-Module Pink-* | Remove-Module;
 pushd $scriptDir;
 try{
-    Get-Module Pink-* | Remove-Module;
+    if($version){
+        $oldVersion = Get-Content Version.txt;
+        $version = .\build\Update-VersionNumber.ps1 $oldVersion -versionNumberPattern:$version;
+    }
 
-    AssembleModule .\build $outputFolder;
-    AssembleModule .\install\SSAS $outputFolder;
+    # Find (script) module manifests to locate modules and build
+    foreach($manifest in Get-ChildItem -Directory -Exclude:output | % { Get-ChildItem $_ *.psd1 -Recurse }){
+        AssembleModule $manifest.FullName $outputFolder -version:$version;
+    }
+    # AssembleModule .\build $outputFolder -version:$version;
+    # AssembleModule .\install\SSAS $outputFolder -version:$version;
 
     # Smoke test the modules
     Write-Host "Smoke Test Get-VSSolutionProjects";
-    Get-VSSolutionProjects -solution:.\Samples\VS2010\SamplesVS2010.sln;
-
-    return;
-    #if($version){
-    #    $version = .\Scripts\Update-VersionNumber.ps1 $version;
-    #}
+    $projects = @(Get-VSSolutionProjects -solution:.\Samples\VS2010\SamplesVS2010.sln);
+    Assert ($projects.Length -eq 1) -successMessage:Ok;
 
     #dir $scriptDir *.nuspec -Recurse | .\Scripts\Set-VersionNumber.ps1 -version:$version;
     #$version | Out-File .\Version.txt -Force;
 
+    <#
     $outDir = Join-Path ([IO.Path]::GetPathRoot($scriptDir)) $packageStoreName;
     if(!(Test-Path $outDir)){
         $void = mkdir $outDir;
@@ -63,6 +87,10 @@ try{
             Write-Error "msbuild.exe failed with $LASTEXITCODE"
             exit $LASTEXITCODE 
         }
+    }
+    #>
+    if($version){
+        Set-Content -Value:$version -Path:Version.txt;
     }
 
 }catch{
