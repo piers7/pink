452f5195cc6ab9c721abd3e63fa9c2b807a5cc05
Piers Williams <piers7@users.noreply.github.com>
2016-02-23 15:41:15 +0800

    Find MSBuild based on msbuild registry key
    ...rather than using .net framework registry key

--- Build/Get-MSBuildPath.ps1
+++ Build/Get-MSBuildPath.ps1
@@ -1,43 +1,29 @@
-<#
-.Synopsis
-Locates the path to MSBuild, given a framework version (or uses the highest installed)
-
-.Notes
-As per VS 2013, MSBuild is be bundled with Visual Studio, not the .Net Framework.
-See http://blogs.msdn.com/b/visualstudio/archive/2013/07/24/msbuild-is-now-part-of-visual-studio.aspx
-However the ToolLocationHelper class (to find it) has *also* moved into VS2013
-https://msdn.microsoft.com/en-us/library/microsoft.build.utilities.toollocationhelper(v=vs.121).aspx
-
-#>
-[CmdLetBinding()]
+﻿# .synopsis Locates the path to MSBuild, based on MSBuild registry keys
 param(
-    # Specify the framework version, or leave blank default to highest located
-    $frameworkVersion, 
-
-    # Force using 32 bit version of MSBuild
-    [switch] $x86
+    $version,
+    [switch]$x86
 )
 
-# very simple implementation that just uses embedded version numbers
-function NumericalSort(){
-    $input | ? { $_ -match '^v(\d+(\.\d+))?' } | % { 
-        New-Object -TypeName:PSObject -Property:@{
-            SortKey = $Matches[1] -as [Float];
-            Value = $_
-        }
-    } | Sort SortKey | Select-Object -ExpandProperty:Value
+$is64Bit = if([intptr]::Size -eq 4) { $true } else { $false };
+
+$keys = if($is64Bit -and $x86){ 
+    dir registry::hklm\SOFTWARE\Wow6432Node\Microsoft\MSBuild\ToolsVersions
+} else { 
+    dir registry::hklm\SOFTWARE\Microsoft\MSBuild\ToolsVersions
+};
+
+$keys = $keys | % { new-object PSObject -Property:@{
+            # Item = $_;
+            Name = $_.PSChildName;
+            Path = $_.GetValue('MSBuildToolsPath');
+        }}
+
+if($version){
+    # Find matching key for the version specified
+    $keys = $keys | ? { $_.Name -eq $version }
+}else{
+    # Find latest key (highest version number)
+    $keys = $keys | Sort-Object -Desc -Property:@{Expression = { [double]::Parse($_.Name) }}
 }
 
-# Resolve framework directory (for MsBuild)
-$frameworkKey = Get-Item "hklm:\software\microsoft\.netframework"
-$frameworkDir = $frameworkKey.GetValue('InstallRoot');
-if(!$frameworkVersion){
-    $frameworkVersion = ($frameworkKey.GetSubKeyNames() | NumericalSort | Select-Object -Last:1).Substring(1);
-}
-if($x86){
-    $frameworkDir = $frameworkDir -replace 'Framework64','Framework';
-}
-$msbuild = Resolve-Path "$frameworkDir\v$frameworkVersion\MSBuild.exe";
-
-Write-Verbose "Using MsBuild from $msbuild";
-$msbuild;
+$keys | Select-Object -First:1 | % { Join-Path $_.Path 'MSBuild.exe' };
