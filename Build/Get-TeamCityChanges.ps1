074e3eab408730095e4b9d331798b48f9097e77e
Piers Williams <male@nospam@piers7.com>
2016-03-16 10:20:48 +0800

    Extend TeamCity rest API support to run on PowerShell v2

--- Build/Get-TeamCityChanges.ps1
+++ Build/Get-TeamCityChanges.ps1
@@ -1,6 +1,6 @@
 <#
 .synopsis
-Extracts checkin comments from TeamCity's REST api for a build
+Extracts checkin comments from TeamCity's REST api for a particular build (normally the current one)
 #>
 [CmdLetBinding()]
 param(
@@ -8,18 +8,34 @@ param(
     $buildId,
     [Parameter(Mandatory=$true)]
     $serverUri = 'http://teamcity:8070',
-    [switch] $asMarkdown
+    [switch] $asMarkdown,
+    [switch] $noProxy
 )
 
 function Load-Xml($uri){
-    $response = Invoke-WebRequest -Uri:$uri;
-    [xml]($response.Content);
+    if(Get-Command Invoke-WebRequest -ErrorAction:SilentlyContinue){
+        $response = Invoke-WebRequest -Uri:$uri;
+        [xml]($response.Content);
+    }else{
+        # Invoke-WebRequest not available on PS 2
+        $client = New-Object System.Net.WebClient
+        if($noProxy){
+	        $client.Proxy = $null;
+        }
+        if($client.Proxy){
+	        Write-Verbose "Retrieving $uri using proxy $($client.Proxy.GetProxy($uri))";
+        }else{
+	        Write-Verbose "Retrieving $uri";
+        }
+        $response = $client.DownloadString($uri);
+        [xml]$response;
+    }
 }
 
 # Nice non-iterative version as per http://stackoverflow.com/a/25515487/26167
-
 $changes = Load-Xml "$serverUri/guestAuth/app/rest/changes?locator=build:(id:$buildId)&fields=count,change:(version,date,username,comment)"
 $changesParsed = $changes.SelectNodes('//change') | Select-Object Version,UserName,@{Name='date';Expression={[DateTime]::ParseExact($_.date, 'yyyyMMddTHHmmsszzzz', $null)}},Comment
+
 if(!$asMarkdown){
     $changesParsed
 }else{
